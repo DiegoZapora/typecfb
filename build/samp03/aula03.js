faa8562418c353efb40d2b52e30dc75757061255
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-07-31 15:00:16 -0300

    gsdgs

--- build/samp03/aula03.js
+++ build/samp03/aula03.js
@@ -2,3 +2,5 @@
 let anome = "Andre";
 anome = "Matos";
 console.log(anome);
+let dados = ["Andre", 47];
+console.log(dados);
--- src/samp03/aula03.ts
+++ src/samp03/aula03.ts
@@ -2,3 +2,7 @@ let anome:string = "Andre"
 anome = "Matos"
 
 console.log(anome)
+
+let dados: readonly[string, number] = ["Andre", 47]
+
+console.log(dados)
