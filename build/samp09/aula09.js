5f727f5cc0e249eca306930774aee3aa95b6842e
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-08-05 14:08:30 -0300

    gdggd

---
+++ build/samp09/aula09.js
@@ -0,0 +1,7 @@
+"use strict";
+let valor9;
+let samp9;
+samp9 = 4;
+valor9 = samp9;
+console.log(samp9);
+console.log(typeof (samp9));
---
+++ src/samp09/aula09.ts
@@ -0,0 +1,9 @@
+let valor9:number
+let samp9:unknown
+
+samp9 = 4
+
+valor9 = <number>samp9
+
+console.log(samp9)
+console.log(typeof(samp9))
